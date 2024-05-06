e5f1ab89cd0b17f0afb47e0143e4b877c122d0c3
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2024-05-06 22:08:32 +0300

    WIP: providers were separated to own file

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,13 +2,29 @@
 
 import { ReactNode } from "react"
 import { SessionProvider } from "next-auth/react"
+import { UserProvider } from '@/contexts/user-context';
+import { LocalizationProvider } from '@/components/core/localization-provider';
+import { ThemeProvider } from '@/components/core/theme-provider/theme-provider';
+import { ReduxProvider } from '@/lib/redux/provider';
 
 interface Props {
     children: ReactNode
 }
 
 const Providers = ({ children }: Props) => {
-    return <SessionProvider >{children}</SessionProvider>
+    return (
+        <SessionProvider >
+            <ReduxProvider>
+                <LocalizationProvider>
+                    <UserProvider>
+                        <ThemeProvider>
+                            {children}
+                        </ThemeProvider>
+                    </UserProvider>
+                </LocalizationProvider>
+            </ReduxProvider>
+        </SessionProvider>
+    )
 }
 
 export default Providers
